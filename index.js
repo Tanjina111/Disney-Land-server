f6302a7a4da5d8b350310ce24b9dcb3bdaf9784b
Tanjina111 <mrs.tanjinaevans@gmail.com>
2021-11-02 22:47:18 +0600

    second

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ async function run() {
 
         const databaseOrder = client.db('orders');
         const orderCollection = databaseOrder.collection('allOrder')
-    console.log('connected');
+    
+        console.log('connected');
+
         //GET API
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find({});
@@ -35,7 +37,6 @@ async function run() {
         // Get data by keys
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            console.log('arin',id);
             const query = { _id: ObjectId(id) }
             const services = await serviceCollection.findOne(query);
             res.send(services);
@@ -48,11 +49,64 @@ async function run() {
             res.json(result);
         })
 
-        //Single User Orders
-        app.get('/orders/:email', async(req, res) => {
-            const result = await orderCollection.find({email: req.params.email}).toArray();
-            res.send(result);
-        })
+        // Add Service
+        app.post("/services", async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.json(result);
+        });
+
+        // Manage Orders
+        app.get("/manageorders", async (req, res) => {
+            const manageOrder = await orderCollection.find({}).toArray();
+            res.send(manageOrder);
+        });
+
+        //Single User Orders*
+        // app.get('/orders/:id', async(req, res) => {
+        //     const result = await orderCollection.find({email: req.params.email}).toArray();
+        //     res.send(result);
+        // })
+
+        app.get("/orders/:id", async (req, res) => {
+            const mail = req.params.email;
+            const orders = await orderCollection.find({ mail }).toArray();
+            res.send(orders);
+        });
+
+
+        // Delete
+        app.delete("/orders/:id", async (req, res) => {
+            const orderId = req.params.id;
+            const query = { _id: ObjectId(orderId) };
+            const orderDelete = await orderCollection.deleteOne(query);
+            res.json(orderDelete);
+        });
+
+        // // Update
+        // app.put("/orders/:id", async (req, res) => {
+        //     const updateId = req.params.id;
+        //     const updatedStatus = req.body;
+            
+        //     const filter = { _id: ObjectId(updateId) };
+        //     const options = { upsert: true };
+        //     const updateDoc = {
+        //         $set: {
+        //             status: updatedStatus.status,
+        //         },
+        //     };
+        //     const approvedres = await orderCollection.updateOne(filter, updateDoc, options);
+        //     res.json(approvedres);
+        // });
+
+        // 
+        // app.post("/placeorders", async (req, res) => {
+        //     const orders = req.body;
+        //     const result = await orderCollection.insertOne(orders);
+        //     res.json(result);
+        // });
+
+             
     }
     finally {
         // await client.close();
@@ -62,7 +116,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Ema jon server is running');
+    res.send('Server is running');
 });
 
 app.listen(port, () => {
